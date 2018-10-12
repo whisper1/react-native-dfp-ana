1868d3d7ed8807d34b8cc6e1711f6ec995b07876
David Jackman <djackman@whisper.sh>
2018-10-12 14:22:13 -0700

    IOS Working
    Remove Pods

--- ios/RNDfpAna.h
+++ ios/RNDfpAna.h
@@ -1,11 +1,14 @@
 
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#import "RCTLog.h"
 #else
 #import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
 #endif
+
+@import GoogleMobileAds;
 
 @interface RNDfpAna : NSObject <RCTBridgeModule>
 
 @end
-  
--- ios/RNDfpAna.m
+++ ios/RNDfpAna.m
@@ -1,5 +1,15 @@
 
 #import "RNDfpAna.h"
+#import "RNDfpAna-Swift.h"
+
+@import whisper;
+//@import Datametrical;
+
+@interface RNDfpAna()
+
+//@property (nonatomic, strong) AnaAdapter * anaAdapter;
+
+@end
 
 @implementation RNDfpAna
 
@@ -7,7 +17,17 @@
 {
     return dispatch_get_main_queue();
 }
+
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(init:(NSString *)uuid :(NSString *)baseURL :(NSInteger)age :(NSString *)gender)
+{
+    [[NSUserDefaults standardUserDefaults] setObject:uuid forKey:@"sh.whisper.UserID"];
+    AnaAdapter.shared.baseURL = baseURL;
+    AnaAdapter.shared.age = @(age);
+    AnaAdapter.shared.gender = gender;
+}
+
+
+
 @end
-  
